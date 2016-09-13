54cc7e71024953bce71c2c334686ba76e5a6b489
zyb <1032461591@qq.com>
2016-09-13 11:54:38 +0800

    2016.9.13-11:54

--- app/src/main/java/com/example/zyb/bestmap/mine/activity/MineActivity.java
+++ app/src/main/java/com/example/zyb/bestmap/mine/activity/MineActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.example.zyb.bestmap.R;
 import com.example.zyb.bestmap.home.HomeActivity;
@@ -15,6 +18,7 @@ import com.example.zyb.bestmap.home.HomeActivity;
  */
 public class MineActivity extends Activity {
     private ImageButton ib_back;
+    private CheckBox cb_night;
     private RadioButton rb_suggest;
     private RadioButton rb_thanks;
 
@@ -24,6 +28,7 @@ public class MineActivity extends Activity {
         setContentView(R.layout.activity_mine);
         //初始化控件
         ib_back = (ImageButton) findViewById(R.id.ib_back);
+        cb_night = (CheckBox) findViewById(R.id.cb_night);
         rb_suggest = (RadioButton) findViewById(R.id.rb_suggest);
         rb_thanks = (RadioButton) findViewById(R.id.rb_thanks);
         //操作返回控件点击事件
@@ -33,6 +38,18 @@ public class MineActivity extends Activity {
                 Intent intent = new Intent(MineActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+        //操作切换夜间模式控件点击事件
+        cb_night.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //判断夜间模式开启与关闭（在这里对应添加关联HomeActivity地图显示模式）
+                if (isChecked) {
+                    Toast.makeText(MineActivity.this, "已开启夜间模式", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MineActivity.this, "已关闭夜间模式", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         //操作跳转帮助与反馈界面控件点击事件
