75bc04e3ccc959b30d3fff287197d782e991cb64
zyb <1032461591@qq.com>
2016-09-14 11:09:35 +0800

    2016.9.14-11:09

--- app/src/main/java/com/example/zyb/bestmap/mine/activity/AgreementActivity.java
+++ app/src/main/java/com/example/zyb/bestmap/mine/activity/AgreementActivity.java
@@ -3,8 +3,10 @@ package com.example.zyb.bestmap.mine.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.zyb.bestmap.R;
 
@@ -13,6 +15,7 @@ import com.example.zyb.bestmap.R;
  */
 public class AgreementActivity extends Activity {
     private ImageButton ib_back;
+    private TextView tv_agreement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,7 @@ public class AgreementActivity extends Activity {
         setContentView(R.layout.activity_agreement);
         //初始化控件
         ib_back = (ImageButton) findViewById(R.id.ib_back);
+        tv_agreement = (TextView) findViewById(R.id.tv_agreement);
         //返回键
         ib_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +33,6 @@ public class AgreementActivity extends Activity {
                 finish();
             }
         });
+        tv_agreement.setMovementMethod(ScrollingMovementMethod.getInstance());
     }
 }
